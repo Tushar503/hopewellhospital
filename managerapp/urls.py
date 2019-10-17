e46a3b8269c4415a449b165e2b3d47455dc52852
vishul chauhan <vishul_chauhan@users.noreply.github.com>
2019-10-17 11:56:29 +0530

    Merge branch 'master' of https://github.com/Tushar503/hopewellhospital
    # Conflicts:
    #	MyUserApp/models.py
    
    changes in email_send.py file
    
    pushing migration initial.py...coz its stopping my pulling process
    
    changes in setting.py file
    
    final signup page
    created admin template and design part
    
    change in manager.html,dapartment.html,its url and function
    
    depttview and deptupdate

--- managerapp/urls.py
+++ managerapp/urls.py
@@ -6,6 +6,8 @@ app_name='managerapp'
 urlpatterns=[
 
     url(r'^manager/$', views.manager, name="manager"),
-    url(r'^department/$',views.departmentadd, name="department")
+    url(r'^department/$',views.departmentadd, name="department"),
+    url(r'^departmentview/$',views.departmentview,name="deparmentview"),
+    url(r'departmentupdate/$',views.departmentupdate,name="departmentupdate"),
 
 ]
