4fb329cce0f8af107a38dfc77aefcd9287550bb6
hp <upasnabhat17@gmail.com>
2019-10-17 10:51:57 +0530

    @upasna4 views.py

--- managerapp/views.py
+++ managerapp/views.py
@@ -6,4 +6,15 @@ from managerapp.models import Department
 def manager(request):
     return render(request,"manager.html")
 
+def departmentadd(request):
+    if request.method=="POST":
+        form=DepartmentForm(request.POST)
+        f=form.save(commit=False)
+        f.depttName= request.POST["department"]
+        f.isActive = True
+        f.save()
+        return render(request, "department.html", {'success': True})
+
+    return render(request, "department.html")
+
 
