fcae556f6dbf68c613f5f23ab842642f43af446f
hp <upasnabhat17@gmail.com>
2019-10-17 12:41:40 +0530

    @upasna4 views.py

--- staffapp/admin.py
+++ staffapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from staffapp.models import Staff
 
 # Register your models here.
+admin.site.register(Staff)
--- staffapp/models.py
+++ staffapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from managerapp.models import Department
+
 
 # Create your models here.
+
+class Staff(models.Model):
+    depttId = models.ForeignKey(Department, on_delete=models.CASCADE)
+    userFullName = models.CharField(max_length=200, default="")
+    userEmail = models.CharField(primary_key=True, max_length=200, default="")
+    userPassword = models.CharField(max_length=200, default="")
+    userMobile = models.CharField(max_length=200, default="")
+    userAge = models.CharField(max_length=10, default="")
+    userAddress = models.CharField(max_length=500, default="")
+    userCity = models.CharField(max_length=10, default="")
+    userState = models.CharField(max_length=200, default="")
+    userImage = models.CharField(max_length=200, default="")
+    isActive = models.BooleanField(default=True)
+    userOtp = models.CharField(max_length=10, default="")
+    userOtptime = models.CharField(max_length=200, default="")
+    #userConfirmationlink = models.CharField(max_length=200, default="")
+    userToken = models.CharField(max_length=200, default="")
+    isAvailable = models.BooleanField(default=True)
+    isQueue = models.BooleanField(default=False)
+    isVerified = models.BooleanField(default=False)
+
